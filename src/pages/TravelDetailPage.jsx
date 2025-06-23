b70ff831c373db6af6f49396c0a4b0d14c7a3ccd
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-23 16:45:51 +0200

    add travel detail card

---
+++ src/pages/TravelDetailPage.jsx
@@ -0,0 +1,49 @@
+import { useParams } from "react-router";
+import { viaggi } from "../data/data";
+
+function TravelDetailPage() {
+  const { id } = useParams();
+  const viaggio = viaggi.find((viaggio) => viaggio.id_viaggio === id);
+  return (
+    <div className="container">
+      <h1>{viaggio.destinazione}</h1>
+      <p>
+        Dal {viaggio.data_inizio} al {viaggio.data_fine}
+      </p>
+      <p>Costo: €{viaggio.costo}</p>
+      <h2>Partecipanti</h2>
+      <div className="row">
+        {viaggio.partecipanti.map((partecipante, index) => (
+          <div key={index} className="accordion" id="accordionExample">
+            <div className="accordion-item">
+              <h2 className="accordion-header">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapse${index}`}
+                  aria-expanded="true"
+                  aria-controls={`collapse${index}`}
+                >
+                  {`${partecipante.nome} ${partecipante.cognome}`}
+                </button>
+              </h2>
+              <div
+                id={`collapse${index}`}
+                className="accordion-collapse collapse"
+                data-bs-parent="#accordionExample"
+              >
+                <div className="accordion-body">
+                  <p>Codice Fiscale: {partecipante.codice_fiscale}</p>
+                  <p>Email: {partecipante.mail}</p>
+                  <p>Numero di telefono: {partecipante.numero_di_telefono}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default TravelDetailPage;
