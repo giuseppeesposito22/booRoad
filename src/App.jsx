97a3506df5c5193985654c141b73c1911d8b1101
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-23 16:07:46 +0200

    add routes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import HomePage from "./pages/HomePage";
+import TravelsPage from "./pages/TravelsPage";
+
 function App() {
   return (
     <>
-      <h1>BooRoad</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/travels" element={<TravelsPage />} />
+            <Route path="/travels/:id" element={""} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
