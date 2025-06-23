abbafbdc34b391ca9ffce47732e89530f33aa4fc
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-23 16:06:57 +0200

    add travel card

---
+++ src/components/TravelCard.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export default function TravelCard({ viaggio }) {
+  return (
+    <>
+      <div className="col">
+        <div className="card">
+          <img src={viaggio.immagine} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{viaggio.destinazione}</h5>
+            <p className="card-text">
+              <span>{viaggio.data_inizio}</span> <br />
+              <span>{viaggio.data_fine}</span>
+            </p>
+            <Link to="" className="btn btn-primary">
+              Vai al dettaglio
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
