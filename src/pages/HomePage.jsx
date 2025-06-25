5f73595d5842a25a970be25e709aa2c0d6b725c2
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-25 10:37:45 +0200

    fix body home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function HomePage() {
   return (
     <>
-      <div className="d-flex flex-column justify-content-center align-items-center vh-100 body-home">
+      <div className="d-flex flex-column justify-content-center align-items-center body-home">
         <h1 className="text-center title my-5">Welcome to BooRoad</h1>
         <Link to="/travels" className="btn btn-danger btn-lg">
           Vai ai nostri viaggi
